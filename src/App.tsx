0b1f01b3cb08fc270ac443b0740d9572e017cb08
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2020-12-28 18:26:10 -0300

    base url

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import GlobalStyle from './styles/global';
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <Router>
+    <Router basename={window.location.pathname || ''}>
       <Routes />
     </Router>
   </>
